8b6f08d515f1bbb6b49e96910483467d4b8b3cf6
Megan Collier <meganraec@live.com>
2018-06-30 23:16:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(open(html))
+  kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
